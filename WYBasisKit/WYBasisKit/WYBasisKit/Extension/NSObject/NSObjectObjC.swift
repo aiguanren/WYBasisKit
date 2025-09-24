aaa76d70469791c4d4f71df848e8c9406a6f54be
官人 <aiguanren@icloud.com>
2025-09-24 11:49:35 +0800

    新增NSObjectObjC

---
+++ WYBasisKit/WYBasisKit/WYBasisKit/Extension/NSObject/NSObjectObjC.swift
@@ -0,0 +1,38 @@
+//
+//  NSObjectObjC.swift
+//  WYBasisKit
+//
+//  Created by guanren on 2025/9/24.
+//
+
+import Foundation
+
+@objc public extension NSObject {
+    
+    /**
+     *  根据传入的superClass获取对应的子类
+     *
+     *  @param superClass 父类
+     *
+     *  @return 获取到的继承自superClass的子类
+     */
+    @objc static func sharedSubClassObjC(superClass: AnyClass) -> [String] {
+        return sharedSubClass(superClass: superClass)
+    }
+    
+    /**
+     *  根据传入的className获取对应的方法列表
+     *
+     *  @param className 类名
+     *
+     *  @return 获取到的className类中的所有方法
+     */
+    @objc static func sharedClassMethodObjC(className: String) -> [String] {
+        return sharedClassMethod(className: className)
+    }
+    
+    /// 获取对象或者类的所有属性和对应的类型(struct类型也适用本方法)
+    @objc static func wy_sharedPropertysObjC(object: Any? = nil, className: String = "") -> [String: Any] {
+        return wy_sharedPropertys(object: object, className: className)
+    }
+}
