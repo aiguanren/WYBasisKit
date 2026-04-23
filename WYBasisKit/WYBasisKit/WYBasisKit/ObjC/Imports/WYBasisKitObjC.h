74a82645f4052bd603b8ac967c5338be33d6d504
官人 <aiguanren@icloud.com>
2026-04-23 13:39:07 +0800

    优化OC头文件import逻辑

--- WYBasisKit/WYBasisKit/WYBasisKit/ObjC/Imports/WYBasisKitObjC.h
+++ WYBasisKit/WYBasisKit/WYBasisKit/ObjC/Imports/WYBasisKitObjC.h
@@ -9,28 +9,44 @@
 
 #if __has_include(<WYBasisKitSwift/WYBasisKitSwift-Swift.h>)
 #import <WYBasisKitSwift/WYBasisKitSwift-Swift.h>
+#elif __has_include("WYBasisKitSwift/WYBasisKitSwift-Swift.h")
+#import "WYBasisKitSwift/WYBasisKitSwift-Swift.h"
 #elif __has_include(<WYBasisKitSwift-Swift.h>)
 #import <WYBasisKitSwift-Swift.h>
+#elif __has_include("WYBasisKitSwift-Swift.h")
+#import "WYBasisKitSwift-Swift.h"
 #endif
 
 #if __has_include(<WYBasisKitObjC/WYBasisKitObjC-Swift.h>)
 #import <WYBasisKitObjC/WYBasisKitObjC-Swift.h>
+#elif __has_include("WYBasisKitObjC/WYBasisKitObjC-Swift.h")
+#import "WYBasisKitObjC/WYBasisKitObjC-Swift.h"
 #elif __has_include(<WYBasisKitObjC-Swift.h>)
 #import <WYBasisKitObjC-Swift.h>
+#elif __has_include("WYBasisKitObjC-Swift.h")
+#import "WYBasisKitObjC-Swift.h"
 #endif
 
 #if __has_include(<WYSingletonMacro.h>)
 #import <WYSingletonMacro.h>
+#elif __has_include("WYSingletonMacro.h")
+#import "WYSingletonMacro.h"
 #endif
 
 #if __has_include(<WYLogManagerMacro.h>)
 #import <WYLogManagerMacro.h>
+#elif __has_include("WYLogManagerMacro.h")
+#import "WYLogManagerMacro.h"
 #endif
 
 #if __has_include(<WYWeakStrongMacros.h>)
 #import <WYWeakStrongMacros.h>
+#elif __has_include("WYWeakStrongMacros.h")
+#import "WYWeakStrongMacros.h"
 #endif
 
 #if __has_include(<WYLocalizableManagerMacro.h>)
 #import <WYLocalizableManagerMacro.h>
+#elif __has_include("WYLocalizableManagerMacro.h")
+#import "WYLocalizableManagerMacro.h"
 #endif
